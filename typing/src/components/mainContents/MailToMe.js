7794ef8f30e5bef2fd62316c1f7664910ebec949
unknown <tndk4997@naver.com>
2024-08-08 23:39:34 +0900

    edit : mail

--- typing/src/components/mainContents/MailToMe.js
+++ typing/src/components/mainContents/MailToMe.js
@@ -2,39 +2,56 @@ import { useState } from "react";
 import emailjs from 'emailjs-com';
 
 function MailToMe() {
+    const [openMailForm, setOpenMailForm] = useState(false)
     const [formData, setFormData] = useState({
-        name : 'sample',
+        name : '박수아',
         email: 'tndk4997@naver.com',
         message : 'sample'
     })
     const handleChange = (e) => {
         const {name, value} = e.target
         setFormData({
-            ...formData
+            ...formData,
+            [name]:value
         })
+        console.log(formData)
     }
     const sendEmail = (e) =>{
-        e.preventDefault();
 
-        const tmeplateParams = {
-            name: "sample",
-            email : "tndk4997@naver.com",
-            message : "sample"
-        }
-        emailjs.send("service_0m0pxcp","template_6hogvdt")
+        emailjs.send("service_0m0pxcp","template_6hogvdt",formData,'yT4NqXc--HGx2KRzE')
         .then((response) => {
             console.log("success",response.status, response.text)
-            setFormData({name:'',email:'',message:''})
+            setFormData()
         },(error) => {
             console.log('FAILED',error)
         })
     }
+    const handleOpenForm = () => {
+        setOpenMailForm(!openMailForm)
+    }
 
     return ( 
         <>
-            <div className="mail-button" onClick={sendEmail}>
+            <div className="mail-button" onClick={handleOpenForm}>
                 <div>의견</div>
                 <div>보내기</div>
+            </div>
+            <div className={openMailForm ? "mail-form-backgrond" : "mail-form-background-close"} onClick={handleOpenForm}>
+            </div>
+            <div className={openMailForm ? "mail-form":"mail-form-close"}>
+                <div>
+                    <h3>소중한 의견을 보내주세요</h3>
+                    <form>
+                        <div>
+                            <input type="text" placeholder="이름" name="name" onChange={handleChange}></input>
+                        </div>
+                        <div>
+                            <input type="textarea" placeholder="내용" name="message" onChange={handleChange}></input>
+                        </div>
+                        <button type="submit" onClick={sendEmail}>보내기</button>
+                    </form>
+                    
+                </div>
             </div>
         </>
      );
